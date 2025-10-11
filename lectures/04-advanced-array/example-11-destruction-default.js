4e95d1cfd553a692210df6b563e3a96e53638d66
Denitsa Mihaylova <p.denica@gmail.com>
2025-10-11 21:32:26 +0300

    Delete lectures/04-advanced-array directory
    Keep only lection 02

--- lectures/04-advanced-array/example-11-destruction-default.js
+++
@@ -1,5 +0,0 @@
-let incompletData = ["api_test", "PASS", 500];
-
-let [testName, testStatus, executionTime = 100] = incompletData;
-
-console.log(`Test: ${testName}, Status: ${testStatus}, Time: ${executionTime}`);
