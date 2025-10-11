dcea4e4f1c9e8d886fade4d834a6c9d3d5e86594
Denitsa Mihaylova <p.denica@gmail.com>
2025-10-11 22:58:43 +0300

    Delete lectures/02-javascript-fundamentals-for-testing directory
    Keep only lection 03

--- lectures/02-javascript-fundamentals-for-testing/examples/example-7-comparison-pitfails.js
+++
@@ -1,9 +0,0 @@
-// Be careful with these!
-console.log("String vs Number:", "5" == 5); // true (loose equality)
-console.log("Strict comparison:", "5" === 5); // false (different types)
-
-// Always use === for testing to avoid surprises
-let userId = "123";
-let expectedId = 123;
-console.log("Loose:", userId == expectedId); // true (but risky!) It is not recomended!
-console.log("Strict:", userId === expectedId); // false (safer!) It's recomended to use strict!
